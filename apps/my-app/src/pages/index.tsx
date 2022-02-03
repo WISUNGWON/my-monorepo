f84ec06a068687a112cde8fbd4e8ba08340d7087
Sungwon WI <52841715+WISUNGWON@users.noreply.github.com>
2022-02-04 00:50:15 +0900

    chore(my-app): src 폴더 생성

---
+++ apps/my-app/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import { Typography } from "@my-monorepo/ui";
+import { AlertIcon } from "@my-monorepo/ui/icons";
+
+interface Props {}
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Typography.Text onClick={() => alert("Hello!"!)} color="gray50">
+        Hello
+      </Typography.Text>
+      <AlertIcon />
+    </>
+  );
+};
+
+export default Home;
